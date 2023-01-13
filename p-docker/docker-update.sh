b67f59f71fd0f58e819bdf8cb24fc4af7871ba52
Chris Cook <chris@darious.co.uk>
2023-01-13 10:06:55 +0000

    Updates to kangtao containers and scripts

--- p-docker/docker-update.sh
+++ p-docker/docker-update.sh
@@ -6,17 +6,5 @@ docker compose pull
 # Fire them all up
 docker compose up -d
 
-# Install ssh in the sonarr container so that the post processing script will work
-docker exec -it sonarr apt install ssh -y
-
-# grab the ssh key for the p-control container so that we can connect
-docker exec -it sonarr sh -c 'ssh-keyscan 192.168.0.236 >> $HOME/.ssh/known_hosts'
-
-
-# make radarr ssh work
-docker exec -it radarr_all apk update
-docker exec -it radarr_all apk --no-cache add openssh-client
-#docker exec -it radarr_all sh -c 'ssh-keyscan 192.168.0.232 >> /config/.ssh/known_hosts'
-
 # finally clean up anything left behind
 docker system prune -af --volumes
