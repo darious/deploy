6426183ae56b66ffde0eeaddf52dee822267b9e4
Christopher Cook <chris@darious.co.uk>
2024-05-25 10:17:09 +0100

    Updated docker pipe create script
    So that it creates the pipe if it doesn't exist and does some better logging

--- p-docker/execdockerpipe.sh
+++ p-docker/execdockerpipe.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-while true; do eval "$(cat /dockerpipe)"; done
+
+PIPE=/dockerpipe
+
+# Create the named pipe if it doesn't exist
+if [[ ! -p $PIPE ]]; then
+    rm -f $PIPE # Remove any existing file or directory with the same name
+    mkfifo $PIPE
+fi
+
+# Log the script activity
+echo "$(date): Starting dockerpipe script" >> /tmp/dockerpipe.log
+
+# Continuously read from the named pipe and evaluate commands
+while true; do
+    if read line < $PIPE; then
+        echo "$(date): Received command: $line" >> /tmp/dockerpipe.log
+        eval "$line"
+    else
+        echo "$(date): Read error: $? - $line" >> /tmp/dockerpipe.log
+    fi
+done
